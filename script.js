641fd92e2f0f311b57dbbb80bc91007b9da6f246
Erin Hollett <erinkhollett@gmail.com>
2025-08-12 17:31:33 -0400

    Add comments

--- script.js
+++ script.js
@@ -21,16 +21,19 @@ class ProductProperties {
 
 // == Part 2: Adding Inheritance == //
 
+// 2.1: Create a Subclass: PerishableProductProperties:
 class PerishableProductProperties extends ProductProperties {
   constructor(name, price, quantity, expirationDate) {
     super(name, price, quantity);
     this.expirationDate = expirationDate;
   }
 
+// 2.2: Methods:
   toString() {
     console.log(`Product: ${this.name}, Price: ${this.price}, Quantity: ${this.quantity}, Expiration Date: ${this.expirationDate}`);
   }
 }
 
+// 2.3: Task: Create at least two instances of this class with sample data:
 let receipt1 = new PerishableProductProperties('Blueberries', 3.99, 2, '2025-08-20');
 let receipt2 = new PerishableProductProperties('Bananas', 2.50, 1, '2025-08-16');
