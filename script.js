3e5291a77cf687a2a023c5904a0486c652dab3b3
Erin Hollett <erinkhollett@gmail.com>
2025-08-12 17:18:34 -0400

    Add comments

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// == Part 1: Setting Up Classess == //
+
+// 1.1: Create a Base Class: ProductProperties:
 class ProductProperties {
   constructor(name, price, quantity) {
     this.name = name;
@@ -5,6 +8,7 @@ class ProductProperties {
     this.quantity = quantity;
   }
 
+// 1.2: Methods:
   getTotalValue() {
     totalValue = this.price * this.quantity;
     console.log(totalValue);
